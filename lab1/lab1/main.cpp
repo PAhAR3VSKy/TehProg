94e047fa6786e4ecea50856135e49f435ed860ad
paharevsky <jo.2015@yandex.ru>
2020-03-06 03:06:19 +0700

    лаба мама не горюй

--- lab1/lab1/main.cpp
+++ lab1/lab1/main.cpp
@@ -1,12 +1,13 @@
-#define _CRT_SECURE_NO_WARNINGS 
-#include <iostream> 
-#include <time.h> 
-#include <stdio.h> 
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
 #include <vector>
-#include <stdlib.h> 
-#include <Windows.h> 
-#define NPAGES 2 
-#define PAGESIZE 512 
+#include <fstream>
+#define NPAGES 2
+#define PAGESIZE 512
 
 using namespace std;
 
@@ -15,62 +16,70 @@ struct VIRTUAL {
 	int Type; /*размер элемента в байтах*/
 	int Status[NPAGES]; /*статус страницы (флаг модификации)*/
 	int Number[NPAGES]; /*номер страницы*/
-	char Page[NPAGES * PAGESIZE]; /*буфер страниц*/
+	char Page[PAGESIZE]; /*буфер страниц*/
 };
 
 VIRTUAL* vini(long size, int Type)
 {
-	VIRTUAL *vp = new VIRTUAL[size];
-	FILE *test;
-	test = fopen("test.txt", "w");
-	srand(time(0));
-	//fwrite(vp->Page, Type, size, test);
-	fclose(test);
+	VIRTUAL* vp = new VIRTUAL[size];
+	vp->Fp = fopen("test.asd", "w+");
+	fclose(vp->Fp);
 	return vp;
 }
-
-void* AllocPage()
-{
-	void* pvAdress;
-	pvAdress = VirtualAlloc(PVOID(NULL), PAGESIZE, MEM_RESERVE, PAGE_READWRITE);
-	return &pvAdress;
-}
-
-template<typename VTYPE>
-int vput(VIRTUAL* array, long index, VTYPE* value)
-{
-	return 0;
-}
-
-template<typename VTYPE>
-int vget(VIRTUAL* array, long index, VTYPE* value)
-{
-	return 0;
-}
-
 void* addres(VIRTUAL* array, long index)
 {
 	vector<int> page;
+	srand(time(0));
 	page.resize(2);
-
-	for (int i = 0; i < sizeof(array) / sizeof(VIRTUAL); i++)
-	{
-		for (int j = 0; j < NPAGES; j++)
+	void* indexElement;
+	long hh = 512;
+	array->Fp = fopen("test.asd", "w+");
+	int page1 = (index - 1) / (PAGESIZE / (sizeof(int) * 8)) + 1;
+	for (int i = 0; i < _msize(array) / sizeof(VIRTUAL); i++)
+		for (int j = 0; j < PAGESIZE; j++)
 		{
-			array[i].Page[j] = index ? page.at(0) = i, page.at(1) = j : 0;
+			array[i].Page[j] == index ? page.at(0) = i, page.at(1) = j : 0;
 		}
-	}
-	if (PVOID(array[page.at(0)].Page[page.at(1)]) != AllocPage)
-	{
-		//array[page.at(0)].Page[page.at(1)] = new (AllocPage()) VIRTUAL;
-	}
+	indexElement = &array[page.at(0)].Page[page.at(1)];
+
+
+
+	return NULL;
+}
+template<typename VTYPE>
+int vput(VIRTUAL* array, long index, VTYPE* value)
+{
+	addres(array, index);
 	return 0;
 }
-
+template<typename VTYPE>
+int vget(VIRTUAL* array, long index, VTYPE* value)
+{
+	addres(array, index);
+	return 0;
+}
+void CreateFile(int count)
+{
+	int random;
+	ofstream file("file.bin", ios::binary | ios::out);
+	srand(time(NULL));
+	for (int i = 0; i < count; i++) {
+		random = rand() % 322;
+		file.write((char*)&random, sizeof(int));
+	}
+	file.close();
+}
 int main()
 {
-	vini(NPAGES, PAGESIZE);
-
+	//addres(vini(NPAGES, PAGESIZE),100);
+	fstream inOut;
+	char s[100];
+	CreateFile(100);
+	inOut.open("file.bin", ios::binary | ios::in);
+	for (int i = 0; i < 100; i++) {
+		inOut >> s[i];
+		cout << s[i];
+	}
+	inOut.close();
 	return 0;
-
 }
