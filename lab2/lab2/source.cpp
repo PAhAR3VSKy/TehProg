92bbfc918e9ec96f83508f4fffab5b0480aa26b5
paharevsky <jo.2015@yandex.ru>
2020-04-21 12:57:58 +0700

    добавил char

--- lab2/lab2/source.cpp
+++ lab2/lab2/source.cpp
@@ -17,14 +17,12 @@ int checkValue()
 	}
 }
 
-
-int main()
+void CreateListInt()
 {
-	setlocale(LC_ALL, "ru");
-	int size;
-	int operation = -1;
-	int value;
 	ListStructure<int>* list = new ListStructure<int>();
+	int size,
+		operation = -1,
+		value;
 	cout << "Введите размер списка:";
 	size = checkValue();
 	list->init(size, "file.bin");
@@ -55,6 +53,68 @@ int main()
 			cout << "Список был сортирован" << endl;
 			break;
 		}
+	}
+}
+
+void CreateListChar()
+{
+	ListStructure<char>* list = new ListStructure<char>();
+	int size,
+		operation = -1;
+	char value;
+	cout << "Введите размер списка:";
+	size = checkValue();
+	list->init(size, "file.bin");
+	list->fromFile();
+	while (operation != 0) {
+		cout << "Выберите операцию:" << endl
+			<< "· 1-вставить значение	·" << endl
+			<< "· 2-удалить значение	·" << endl
+			<< "· 3-сортировать список	·" << endl;
+		operation = checkValue();
+		switch (operation)
+		{
+		case 1:
+			cout << "Введите значение для вставки в список: ";
+			cin >> value;
+			list->insert(value);
+			list->fromFile();
+			break;
+		case 2:
+			cout << "Введите значение для удаления из списка: ";
+			cin >> value;
+			list->del(value);
+			list->fromFile();
+			break;
+		case 3:
+			list->pack();
+			list->fromFile();
+			cout << "Список был сортирован" << endl;
+			break;
+		}
+	}
+}
+
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	int choice;
+	cout << "Выбирите тип данных:" << endl
+		 << "1. Int" << endl
+		 << "2. Char" << endl;
+	cin >> choice;
+	switch (choice)
+	{
+	case 1:
+		CreateListInt();
+		break;
+	case 2:
+		CreateListChar();
+		break;
+	default:
+		cout << "Неверно введено значение" << endl;
+		break;
 	}
 	return 0;
 }
